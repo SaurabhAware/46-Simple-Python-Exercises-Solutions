1f8c165a2c8befd123dd35ad5db41eee7da0642b
Saurabh_Aware <76786574+SaurabhAware@users.noreply.github.com>
2022-05-15 01:41:53 +0530

    Update problem_25.py
    to satisfy lie,die and tie as lying,dying,tying(https://www.grammar.cl/Notes/Spelling_ING.htm)

--- problem_25.py
+++ problem_25.py
@@ -25,15 +25,14 @@ def make_ing_form(passed_string):
     consonant = [c for c in letter if c not in vowel]
     exception = ['be', 'see', 'flee', 'knee', 'lie']
     if passed_string.endswith('e'):
-        if passed_string in exception:
+        if passed_string.endswith('ie'):
+            passed_string = passed_string[:-2]
+            return passed_string + 'ying'
+        elif passed_string in exception:
             return passed_string + 'ing'
         else:
             passed_string = passed_string[:-1]
             return passed_string + 'ing'
-
-    elif passed_string.endswith('ie'):
-        passed_string = passed_string[:-2]
-        return passed_string + 'ying'
 
     elif passed_string[-1] in consonant and passed_string[-2] in vowel and passed_string[-3] in consonant:
         passed_string += passed_string[-1]
